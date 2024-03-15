42496defd5cd568fc6bd5a9ca4f7470a5f6311e4
Mikkel Pedersen <mkklpdrsn@gmail.com>
2024-03-15 14:53:51 +0100

    docs(icon): Update recipe icon

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setuptools.setup(
     setup_requires=['setuptools_scm'],
     url='https://github.com/pollination/leed-daylight-option-one',                   # will be translated to home
     project_urls={
-        'icon': 'https://raw.githubusercontent.com/ladybug-tools/artwork/master/icons_components/honeybee/png/annualrecipe.png',
+        'icon': 'https://raw.githubusercontent.com/ladybug-tools/artwork/master/icons_components/honeybee/png/leeddaylight.png',
         'docker': 'https://hub.docker.com/r/ladybugtools/honeybee-radiance'
     },
     description='LEED daylight option I recipe for Pollination.',                  # will be used as package description
